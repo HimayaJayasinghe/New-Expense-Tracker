504c6b408dcc02fc8b1ce721bbfde79708b23a2a
HimayaJayasinghe <hbinushi@gmail.com>
2025-08-11 17:29:25 +0000

    feat: add AuthLayout component with responsive design and stats info card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Login from './pages/Auth/Login';
+import Signup from './pages/Auth/Signup';
+import Home from './pages/dashboard/Home';
+import Income from './pages/dashboard/Income';
+import Expense from './pages/dashboard/Expense';
 
 const App = () => {
   return (
-    <div className ='text-2xl text-red-600' >App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path = "/" element = {<Root/>} />
+          <Route path ="/login" exact element ={<Login/>} />
+          <Route path = "/signup" exact element ={<Signup/>}/>
+          <Route path = "/dashboard" exact element ={<Home/>}/>
+          <Route path = "/income" exact element ={<Income/>}/>
+          <Route path = "/expense" exact element ={<Expense/>}/>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
 export default App
+
+const Root = () => {
+  //check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //REdirect to dashoard if authenticated , otherwise to login
+  return isAuthenticated ?(
+    <Navigate to ="/dashboard"/>
+  ):(
+    <Navigate to ="/login" />
+
+  )
+  
+
+}
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import AuthLayout from '../../components/layout/AuthLayout'
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+    <AuthLayout>
+      <div className = "lg:w-[78%] h-3/4 md:h-full flex-col justify-center ">
+        <h3 className ="text-xl font-semibold text-black">Welcome back</h3>
+        <p className ="text-xs text-slate-700 mt-[5px] mb-6">please enter your details to log in </p>
+      </div>
+    </AuthLayout>
   )
 }
 
